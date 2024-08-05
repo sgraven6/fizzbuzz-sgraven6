de6d9cd844777c16b1a1715e8971c190124e7996
Your Name <sgraven6>
2024-08-05 00:03:01 -0400

    for x in range (1, 101):     if x % 3 == 0 and x % == 0:         print ("FizzBuzz")     elif x % 3 == 0:         print ("Fizz")     elif x % 5 == 0:         print ("Buzz")     else:         print (x)

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,2 +1,12 @@
-# add your code here
+for x in range (1, 101):
+    if x % 3 == 0 and x % == 0:
+        print ("FizzBuzz")
+    elif x % 3 == 0:
+        print ("Fizz")
+    elif x % 5 == 0:
+        print ("Buzz")
+    else:
+        print (x) 
+        
+        
 
